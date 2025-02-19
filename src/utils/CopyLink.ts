2b3c5665d0bb6b10ebf9edc2cfd15f968e018f78
datnguyex <nnguyendat72@gmail.com>
2025-02-19 18:21:38 +0700

    update first page

--- src/utils/CopyLink.ts
+++ src/utils/CopyLink.ts
@@ -8,7 +8,7 @@ export const CopyLink = ({ ref }: CopyLinkProps) => {
     if (ref.current) {
         ref.current.select();
         document.execCommand('copy');
-        console.log('Copied to clipboard!');
+        console.log('Copied to clipboard!!');
     } else {
         console.error('Input element not found');
     }
